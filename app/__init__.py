2bac20badd80e48f992808789593bfb4ab1fb96b
kaphelps33 <alexphelps2001@gmail.com>
2023-11-09 17:17:31 -0500

    Hook up to local databse + login function

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
+    # TODO: THIS SHOULD BE HIDDEN IN PRODUCTION
     app.config["SECRET_KEY"] = "key"
+
+    # Initialize DB
+
+    # Add MySQL Database
+    # TODO: HIDE IN PRODUCTION
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "mysql+pymysql://root:nevereatsoggywafflesclock@localhost/planify"
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import Users
 
     return app
--- app/auth.py
+++ app/auth.py
@@ -1,11 +1,61 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Users
+from flask_sqlalchemy import SQLAlchemy
+from . import db, app
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    login_required,
+    logout_user,
+    current_user,
+)
+
 
 auth = Blueprint("auth", __name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            # Check Hash
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login Successful")
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Wrong Password")
+        else:
+            flash("That user does not exist! Try again!")
+    return render_template("login.html", form=form)
+
+
+@auth.route("/dashboard", methods=["GET", "POST"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @auth.route("/logout")
@@ -15,22 +65,37 @@ def logout():
 
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    if request.method == "POST":
-        email = request.form.get("email")
-        first_name = request.form.get("first_name")
-        password = request.form.get("password-1")
-        password_two = request.form.get("password-2")
+    name = None
+    form = UserForm()
 
-        if len(email) < 4:
-            flash("Email must be more than 3 characters!", category="error")
-        elif len(first_name) < 2:
-            flash("First name must be more than 1 character!", category="error")
-        elif password != password_two:
-            flash("Passwords do not match!", category="error")
-        elif len(password) < 7:
-            flash("Password must be at least 7 characters", category="error")
-        else:
-            flash("Account created successfully!", category="success")
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(
+                name=form.name.data,
+                username=form.username.data,
+                email=form.email.data,
+                password_hash=form.password_hash.data,
+            )
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.username.data = ""
+        form.email.data = ""
+        form.password_hash.data = ""
+        flash("User Successfully Created!")
+    return render_template("sign_up.html", name=name, form=form)
 
-    return render_template("sign_up.html")
 
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password_hash = PasswordField(
+        "Password", validators=[Length(min=8, message="Password to short!")]
+    )
+    confirm = PasswordField(
+        "Confirm Password", validators=[EqualTo("password_hash", "Password Mismatch")]
+    )
+    submit = SubmitField("Submit")
