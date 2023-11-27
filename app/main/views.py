670f76a6b356b47c5c347abc94f71544fae147e9
kaphelps33 <alexphelps2001@gmail.com>
2023-11-27 16:10:12 -0500

    course display

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from . import main
 from .forms import add_course_form
-from flask import render_template
+from ..extensions import mongo
+from flask import render_template, request
 from flask_login import login_required
 
 
@@ -10,7 +11,24 @@ def index():
 
 
 @main.route("/dashboard", methods=["GET", "POST"])
-@login_required
+# @login_required
 def dashboard():
     form = add_course_form()
     return render_template("dashboard.html", form=form)
+
+
+@main.route("/dashboard", methods=["GET", "POST"])
+@main.route("/dashboard/search")
+def search():
+    form = add_course_form()
+    q = request.args.get("q")
+
+    if q:
+        results = list(mongo.db.courses.find({"code": {"$regex": "q"}}))
+    else:
+        results = []
+
+    print(q)
+    print(results)
+
+    return render_template("dashboard.html", courses=results, form=form)
