55555eeae3b56f252f523c1d63d6f5d010422896
kaphelps33 <alexphelps2001@gmail.com>
2023-12-10 15:10:18 -0500

    Clean up repo

--- app/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-api = Blueprint("auth", __name__)
-
-from . import views
--- app/api/views.py
+++
@@ -1,9 +0,0 @@
-from . import api
-from ..main.forms import AddCourseForm
-from flask import request
-
-
-@api.route("/get-location/")
-def get_location():
-    form = AddCourseForm()
-    location = request.form.get("")
